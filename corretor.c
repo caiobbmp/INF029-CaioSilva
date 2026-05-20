a41761f6f3da4dc533024858f32ba1bc399fadc8
caio <31149173+caiobbmp@users.noreply.github.com>
2026-05-20 02:18:24 +0000

    Finalizando

--- corretor.c
+++ corretor.c
@@ -18,7 +18,7 @@
 #include <string.h>
 
 //renomeie o arquivo do include abaixo para trabalho1.h
-#include "trabalho1.h"
+#include "CaioAraujo-20242160033-T1.h"
 
 void testSomar();    //função utilizada para testes
 void testFatorial(); //função utilizada para testes
@@ -34,13 +34,13 @@ int main()
 {
     testSomar();
     testFatorial();
-    /*testQ1();
+    testQ1();
     testQ2();
     testQ3();
     testQ4();
     testQ5();
     testQ6();
-    testQ7();*/
+    testQ7();
 }
 
 int ligado = 0;
